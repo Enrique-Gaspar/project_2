ff18b864c3a5dacfb0a27dba379f4fac26aa8306
alfredovelarde <velardenalda@gmail.com>
2020-04-30 19:38:47 -0500

    Update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import datetime as dt
 
+import sqlathanor
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -11,7 +12,7 @@ from flask import Flask, jsonify
 # Database Setup
 
 Base = automap_base()
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/covid19")
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/covid19") 
 Base.prepare(engine, reflect=True)
 session = Session(engine)
 covidData = Base.classes.covid19
@@ -23,7 +24,13 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     
-    return (session, covidData)
+    db_list = session.query(covidData).all()
+    result = [d.__dict__ for d in db_list]
+    for r in result:
+        r.pop('_sa_instance_state')
+    print (result)
+    return jsonify(result=result)
+
 
 
 if __name__ == '__main__':
