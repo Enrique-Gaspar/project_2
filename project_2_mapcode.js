fe9b2a9a50ecf77a1760c82c3a4147ad93cc83e5
HeCoSe92 <hector_cs91@hotmail.com>
2020-05-01 15:18:33 -0500

    update

---
+++ project_2_mapcode.js
@@ -0,0 +1,18 @@
+ <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=ESCRIBE_AQUI_TU_API_KEY&callback=initMap">
+    </script>
+var marker = new google.maps.Marker({
+          position: {lat: 43.542194, lng: -5.676875},
+          map: map,
+	  title: 'Acuario de Gijón'
+        });
+JavaScript.js
+mapcode.js
+   <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=ESCRIBE_AQUI_TU_API_KEY&callback=initMap">
+    </script>
+        var marker = new google.maps.Marker({
+          position: {lat: 43.542194, lng: -5.676875},
+          map: map,
+	  title: 'Acuario de Gijón'
+        });
