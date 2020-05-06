bf9021623da743b03ad20034094ece20286cdb3f
Enrique Gaspar <enrique.gaspar.md@gmail.com>
2020-05-05 21:03:07 -0500

    Last version presneted in class. EG.

--- final/app.py
+++ final/app.py
@@ -7,22 +7,20 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 # Database Setup
 Base = automap_base()
-engine = create_engine("postgresql://postgres:postgres@localhost:5433/henrigaspard") 
+engine = create_engine("postgresql://postgres:postgres@localhost:5433/henrigaspard")
 Base.prepare(engine, reflect=True)
 session = Session(engine)
-covidData = Base.classes 
-#print (engine.table_names())
+covidData = Base.classes
 
 # Flask Setup
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    
     db_list = session.query(covidData).all()
     result = [d.__dict__ for d in db_list]
     for r in result:
@@ -30,6 +28,21 @@ def welcome():
     print (result)
     return jsonify(result=result)
 
+@app.route("/home")
+def main():
+    return render_template('main.html')
+
+@app.route("/inpatients_per_date")
+def graphinpatients():
+    return render_template('inpatients_per_date.html')
+
+@app.route("/deaths_per_date")
+def graphdeaths():
+    return render_template('deaths_per_date.html')
+
+@app.route("/subjects_per_state")
+def mexicomap():
+    return render_template('subjects_per_state.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
